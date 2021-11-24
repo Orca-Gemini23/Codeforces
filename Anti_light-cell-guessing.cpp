bf5041e2d41e511a1982321d0efdf1bbfcdcead2
Orca Gemini <cs.yash95@gmail.com>
2021-11-24 10:26:02 +0530

    filrst commit

---
+++ Anti_light-cell-guessing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int n, m, t;
+    int arr[n][m];
+    int count = 0;
+    cin >> t;
+    while(t--){
+        cin >> n >> m;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            count += 
+        }
+    }
+    return 0;
+}
